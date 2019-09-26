ad3fdc34fa571c83ce7e064ff850c6fb00281a0d
yomihiko <44966734+yomihiko@users.noreply.github.com>
2019-09-26 17:11:02 +0900

    画面完成

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,10 @@
 package application;
-	
+
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
-import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
 
 
 public class Main extends Application {
@@ -15,12 +15,14 @@ public class Main extends Application {
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.setResizable(false);
+			primaryStage.setTitle("命名補助ツール");
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
--- src/application/TopController.java
+++ src/application/TopController.java
@@ -1,5 +1,49 @@
 package application;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+
 public class TopController {
-	
+
+    @FXML
+    private GridPane mainPane;
+
+    @FXML
+    private HBox titleBox;
+
+    @FXML
+    private Label titleLabel;
+
+    @FXML
+    private Label word1Label;
+
+    @FXML
+    private Label word2Label;
+
+    @FXML
+    private Label partsLabel;
+
+    @FXML
+    private ChoiceBox<?> word1Select;
+
+    @FXML
+    private ChoiceBox<?> word2Select;
+
+    @FXML
+    private ChoiceBox<?> partsSelect;
+
+    @FXML
+    private HBox resultBox;
+
+    @FXML
+    private TextField resultForm;
+
+    @FXML
+    private Button copyBtn;
+
 }
